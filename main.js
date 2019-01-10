8219dec918b39f2eb382259b6c16e5a730f193e3
lin.yen[顏琳] <lin.yen@104.com.tw>
2019-01-10 11:42:53 +0800

    modified

--- main.js
+++ main.js
@@ -33,6 +33,10 @@ icon.addEventListener('click',function(e){
   //   e.stopImmediatePropagation();
   // }, true)
 
+  document.body.addEventListener('touchmove', function (e) {
+    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+
 });
 
 
